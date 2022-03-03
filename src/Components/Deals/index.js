20bb9c9065a03c19ade546371613f6761c97dc73
nilanjan <nsinhababu@outlook.com>
2022-03-03 23:43:30 +0530

    restructured deals component

--- src/Components/Deals/index.js
+++ src/Components/Deals/index.js
@@ -1,7 +1,20 @@
 const Deals = () => {
   return (
     <div className='deals-background'>
-      <div className='deals'></div>
+      <div className='deals'>
+        <div className='deals-hdng'></div>
+
+        <div className='deals-container'>
+          <div className='deals-img-container'>
+            <img src='' alt='' className='deals-img' />
+          </div>
+
+          <div className='deals-content-container'>
+            <div className='deals-content-one'></div>
+            <div className='deals-content-two'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Main/card.js
+++ src/Components/Main/card.js
@@ -9,16 +9,18 @@ export const WeeklyCard = () => {
 
 const Card = () => {
   return (
-    <div className='card__container'>
-      <div>
-        <img src='' alt='' className='card__img' />
+    <a href=''>
+      <div className='card__container'>
+        <div>
+          <img src='' alt='' className='card__img' />
+        </div>
+        <div className='card__content-container'>
+          <h2 className='card__hdng'>New News</h2>
+          <div className='card__author'></div>
+          <div className='card__content'></div>
+        </div>
       </div>
-      <div className='card__content-container'>
-        <h2 className='card__hdng'>New News</h2>
-        <div className='card__author'></div>
-        <div className='card__content'></div>
-      </div>
-    </div>
+    </a>
   );
 };
 
